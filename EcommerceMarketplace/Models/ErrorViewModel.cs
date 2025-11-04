86d4cdf2ca637ab5a97198b2cb5e25366575efc9
lcvaz <lucasvaz278@gmail.com>
2025-11-04 19:06:55 -0300

    primeiro commit comando: dotnet new mvc -n EcommerceMarketplace

---
+++ EcommerceMarketplace/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EcommerceMarketplace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
