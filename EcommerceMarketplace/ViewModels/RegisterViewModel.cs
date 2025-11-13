2d2ad2e438c6bcc49125b30caa11c49f62662713
Lucas Santos Vaz <lvaz@MKMONDIAL.local>
2025-11-13 16:31:09 -0300

    Adiciona o DTO RegisterViewModel

---
+++ EcommerceMarketplace/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceMarketplace.ViewModels;
+
+///<summary>
+/// Esta classe é um DTO
+///</summary>
+public class RegisterViewModel 
+{
+    public string Nome { get; set;} = string.Empty;
+}
